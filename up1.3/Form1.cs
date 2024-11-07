eee2d3651fb8fa505119cc603b2ccfeaeb588a51
Никита Белых <belih135nik89@gmail.comm>
2024-11-07 11:35:26 +0500

    m2

---
+++ up1.3/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace up1._3
+{
+    public partial class Form1 : Form
+    {
+        private Playlist playlist;
+        public Form1()
+        {
+            InitializeComponent();
+            playlist = new Playlist();
+        }
+        // Добавление аудиозаписи
+        private void AddSong(object sender, EventArgs e)
+        {
+            try
+            {
+                string author = textBox1.Text;
+                string title = textBox2.Text;
+                string filename = textBox3.Text;
+                playlist.AddSong(author, title, filename);
+                UpdateCurrentSong();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Переход к следующей аудиозаписи
+        private void NextSong(object sender, EventArgs e)
+        {
+            try
+            {
+                playlist.NextSong();
+                UpdateCurrentSong();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Переход к прошлой аудиозаписи
+        private void PreviousSong(object sender, EventArgs e)
+        {
+            try
+            {
+                playlist.PreviousSong();
+                UpdateCurrentSong();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Переход к аудиозаписи по индексу
+        private void GoToSong(object sender, EventArgs e)
+        {
+            if (int.TryParse(numericUpDown1.Value.ToString(), out int index))
+            {
+                try
+                {
+                    playlist.GoToSong(index - 1);
+                    UpdateCurrentSong();
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите корректный индекс!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Переход к первой аудиозаписи
+        private void GoToStart(object sender, EventArgs e)
+        {
+            playlist.GoToStart();
+            UpdateCurrentSong();
+        }
+        // Удаление аудиозаписи из списка по индексу
+        private void RemoveSong(object sender, EventArgs e)
+        {
+            if (int.TryParse(numericUpDown2.Value.ToString(), out int index))
+            {
+                try
+                {
+                    playlist.RemoveSong(index - 1);
+                    UpdateCurrentSong();
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите корректный индекс!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Очистка всего списка 
+        private void ClearPlaylist(object sender, EventArgs e)
+        {
+            playlist.ClearPlaylist();
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+        // Обновление списка
+        private void UpdateCurrentSong()
+        {
+            try
+            {
+                Song currentSong = playlist.CurrentSong();
+                textBox4.Text = currentSong.Author;
+                textBox5.Text = currentSong.Title;
+                textBox6.Text = currentSong.Filename;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ up1.3/Playlist.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace up1._3
+{
+    public struct Song
+    {
+        public string Author;
+        public string Title;
+        public string Filename;
+    }
+    public class Playlist
+    {
+        private List<Song> list;
+        private int currentIndex;
+
+        public Playlist()
+        {
+            list = new List<Song>();
+            currentIndex = 0;
+        }
+
+        public Song CurrentSong()
+        {
+            if (list.Count > 0)
+                return list[currentIndex];
+            else
+                throw new IndexOutOfRangeException("Невозможно получить текущую аудиозапись для пустого плейлиста!");
+        }
+
+        // Добавление аудиозаписи
+        public void AddSong(Song song)
+        {
+            // Проверка на пустые поля
+            if (song.Author == " " || song.Author == "" || song.Title == " " || song.Title == "" || song.Filename == " " || song.Filename == "")
+            {
+                throw new ArgumentException("Все поля песни должны быть заполнены!");
+            }
+            else
+            // Проверка на то, что такой аудиозаписи еще нет в списке
+            if (list.Contains(song) || list.FindIndex(s => s.Author == song.Author && s.Title == song.Title && s.Filename == song.Filename) != -1)
+            {
+                throw new InvalidOperationException("Такая песня уже существует в плейлисте!");
+            }
+            else
+                list.Add(song);
+        }
+
+        public void AddSong(string author, string title, string filename)
+        {
+            Song song = new Song { Author = author, Title = title, Filename = filename };
+            AddSong(song);
+        }
+
+        // Переход к следующей песне
+        public void NextSong()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Плейлист пуст!");
+            }
+            currentIndex = (currentIndex + 1) % list.Count;
+        }
+
+        // Переход к предыдущей песне
+        public void PreviousSong()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Плейлист пуст!");
+            }
+            currentIndex = (currentIndex - 1 + list.Count) % list.Count;
+        }
+
+        // Переход по индексу
+        public void GoToSong(int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Индекс выходит за пределы плейлиста!");
+            }
+            currentIndex = index;
+        }
+
+        // Переход к началу списка
+        public void GoToStart()
+        {
+            if (list.Count > 0)
+            {
+                currentIndex = 0;
+            }
+        }
+
+        // Удаление композиции по индексу
+        public void RemoveSong(int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Индекс выходит за пределы плейлиста!");
+            }
+            list.RemoveAt(index);
+            if (currentIndex >= list.Count)
+            {
+                currentIndex = list.Count - 1;
+            }
+        }
+
+        // Удаление композиции по значению
+        public void RemoveSong(Song song)
+        {
+            int index = list.FindIndex(s => s.Author == song.Author && s.Title == song.Title && s.Filename == song.Filename);
+            if (index != -1)
+            {
+                list.RemoveAt(index);
+
+                if (currentIndex >= list.Count)
+                {
+                    currentIndex = list.Count - 1;
+                }
+            }
+        }
+
+        // Очистка плейлиста
+        public void ClearPlaylist()
+        {
+            list.Clear();
+            currentIndex = 0;
+        }
+    }
+}
